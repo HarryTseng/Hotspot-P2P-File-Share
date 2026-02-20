6dcad002921cc6d0496eec0afea5def25cb2ad0c
HarryTseng <harry22221166@gmail.com>
2026-02-20 12:03:04 +0800

    feat: discover devices

---
+++ udp/client.py
@@ -0,0 +1,18 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+client.settimeout(1.0)
+MESSAGE = b"MOBILE_DEVICE"
+addr = ('255.255.255.255', 8080)
+
+try:
+    client.sendto(MESSAGE, addr)
+    data, server = client.recvfrom(1024)
+    print("Receive")
+
+except socket.timeout:
+    print("Time Exceed")
+
+finally:
+    client.close()
---
+++ udp/server.py
@@ -0,0 +1,13 @@
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind(('', 8080))
+
+while True:
+    data, addr = server.recvfrom(1024)
+    if data == b"MOBILE_DEVICE":
+        server.sendto(b"LAPTOP_REPLY", addr)
+        break
+
+
+
