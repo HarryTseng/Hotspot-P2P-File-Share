18c082d87b1ccc4c9d63e6c5fed76c126dbd38a4
HarryTseng <harry22221166@gmail.com>
2026-03-01 16:51:26 +0800

    feat: TCP file transfer

--- client.py
+++ client.py
@@ -1,5 +1,9 @@
 import socket
+import os
+import tkinter as tk
+from tkinter import filedialog
 
+#UDP discovery
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 client.settimeout(1.0)
@@ -11,9 +15,41 @@ try:
     data, server = client.recvfrom(1024)
     if data == b"LAPTOP_REPLY":
         print("Receive")
-
+        
 except socket.timeout:
     print("Time Exceed")
 
 finally:
     client.close()
+
+server_ip = server[0]
+
+#TCP file transfer
+if server_ip:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((server_ip, 8081))
+
+    #以視窗選擇要傳的檔案
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename()
+    root.destroy()
+
+    #檔案資訊
+    file = open(file_path, "rb")
+    file_name = os.path.basename(file_path)
+    file_size = os.path.getsize(file_path)
+
+    header = f"{file_name}|{file_size}"
+    header_padded = header.encode().ljust(1024)
+    client.sendall(header_padded)
+
+    #傳檔案
+    data = file.read()
+    client.sendall(data)
+
+    file.close()
+    client.close()
+
+else:
+    exit()
--- server.py
+++ server.py
@@ -1,13 +1,48 @@
 import socket
 
+#UDP discovery
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(('', 8080))
 
 while True:
-    data, addr = server.recvfrom(1024)
-    if data == b"MOBILE_DEVICE":
+    conn, addr = server.recvfrom(1024)
+    if conn == b"MOBILE_DEVICE":
         print("Connected")
         server.sendto(b"LAPTOP_REPLY", addr)
+        break
 
+server.close()
 
+#TCP connection
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('', 8081))
+server.listen(1)
 
+conn, addr = server.accept()
+print("TCP connected")
+
+#接收檔案資訊
+header_data = b""
+while len(header_data) < 1024:
+    chunk = conn.recv(1024 - len(header_data))
+    if not chunk:
+        break
+    header_data += chunk
+
+header = header_data.decode().strip()
+file_name, file_size = header.split('|')
+file_size  = int(file_size)
+
+#接收導案
+with open(f"received_{file_name}", "wb") as f:
+    received_size = 0
+    while received_size < file_size:
+        to_read = min(4096, file_size - received_size)
+        data = conn.recv(to_read)
+        if not data:
+            break
+        f.write(data)
+        received_size += len(data)
+
+conn.close()
+server.close()
